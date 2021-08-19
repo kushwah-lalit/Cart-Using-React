f4d1944d4db7c054d1163f4dc9938ab9501afc2d
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-08-19 14:45:21 +0530

    added cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+// import React from 'react';
+// import CartItem from './CartItem';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Cart</h1>
+//       <CartItem />
+//     </div>
+//   );
+// }
+
+// export default App;
 import React from 'react';
-import CartItem from './CartItem';
+import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
-      <CartItem />
+      <Cart />
     </div>
   );
 }
 
 export default App;
+
