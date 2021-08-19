95812c7ab63a53918d41572bdd736df8e639efdf
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-08-20 00:53:53 +0530

    created delete functionality also made required changes

--- src/Cart.js
+++ src/Cart.js
@@ -81,6 +81,15 @@ class Cart extends React.Component {
       products
     })
   }
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id); // [{}]
+
+    this.setState({
+      products: items
+    })
+  }
   render () {
     const { products } = this.state;
     return (
@@ -92,6 +101,7 @@ class Cart extends React.Component {
               key={product.id}
               onIncreaseQuantity={this.handleIncreaseQuantity}
               onDecreaseQuantity={this.handleDecreaseQuantity}
+              onDeleteProduct={this.handleDeleteProduct}
             />
           )
         })}
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,68 +1,69 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  // constructor () {
-  //   super();
-  //   this.state = {
-  //     price: 999,
-  //     title: 'Mobile Phone',
-  //     qty: 1,
-  //     img: ''
-  //   }
-  //   // this.increaseQuantity = this.increaseQuantity.bind(this);
-  //   // this.testing();
-  // }
+  // // constructor () {
+  // //   super();
+  // //   this.state = {
+  // //     price: 999,
+  // //     title: 'Mobile Phone',
+  // //     qty: 1,
+  // //     img: ''
+  // //   }
+  // //   // this.increaseQuantity = this.increaseQuantity.bind(this);
+  // //   // this.testing();
+  // // }
 
-  // testing () {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve('done');
-  //     }, 5000);
-  //   })
+  // // testing () {
+  // //   const promise = new Promise((resolve, reject) => {
+  // //     setTimeout(() => {
+  // //       resolve('done');
+  // //     }, 5000);
+  // //   })
 
-  //   promise.then(() => {
-  //     // setState acts like a synchronus call
-  //     this.setState({ qty: this.state.qty + 10 });
+  // //   promise.then(() => {
+  // //     // setState acts like a synchronus call
+  // //     this.setState({ qty: this.state.qty + 10 });
 
-  //     this.setState({ qty: this.state.qty + 10 });
+  // //     this.setState({ qty: this.state.qty + 10 });
 
-  //     this.setState({ qty: this.state.qty + 10 });
+  // //     this.setState({ qty: this.state.qty + 10 });
 
-  //     console.log('state', this.state);
+  // //     console.log('state', this.state);
+  // //   });
+  // // }
+  // increaseQuantity = () => {
+  //   // this.state.qty += 1;
+  //   // console.log('this', this.state);
+  //   // setState form 1
+  //   // this.setState({
+  //   //   qty: this.state.qty + 1
+  //   // }, () => {});
+
+  //   // setState form 2 - if prevState required use this
+  //   this.setState((prevState) => {
+  //     return {
+  //       qty: prevState.qty + 1
+  //     }
   //   });
   // }
-  increaseQuantity = () => {
-    // this.state.qty += 1;
-    // console.log('this', this.state);
-    // setState form 1
-    // this.setState({
-    //   qty: this.state.qty + 1
-    // }, () => {});
 
-    // setState form 2 - if prevState required use this
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1
-      }
-    });
-  }
+  // decreaseQuantity = () => {
+  //   const { qty } = this.state;
 
-  decreaseQuantity = () => {
-    const { qty } = this.state;
-
-    if (qty === 0) {
-      return;
-    }
-    // setState form 2 - if prevState required use this
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty - 1
-      }
-    });
-  }
+  //   if (qty === 0) {
+  //     return;
+  //   }
+  //   // setState form 2 - if prevState required use this
+  //   this.setState((prevState) => {
+  //     return {
+  //       qty: prevState.qty - 1
+  //     }
+  //   });
+  // }
   render () {
     console.log('render',this.props);
     const { price, title, qty } = this.props.product;
+    const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } = this.props;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -79,19 +80,20 @@ class CartItem extends React.Component {
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/992/992651.svg"
               // onClick={this.increaseQuantity}
-              onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
               // onClick={this.decreaseQuantity}
-              onClick={() => this.props.onDecreaseQuantity(this.props.product)}
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+              onClick={() => onDeleteProduct(product.id)}
             />
           </div>
         </div>
