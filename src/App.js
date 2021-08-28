cbd7184993fa28b39f9a8a4e17844c245076c6e9
lalit kushwah <lalitkushwah@lalits-MacBook-Pro.local>
2021-08-29 02:41:16 +0530

    setup firebase, created db at firebase online,wrote config in index.js,also fetching data from the db in app.js(firebase code as per older version)

--- src/App.js
+++ src/App.js
@@ -15,40 +15,79 @@ import react from 'react';
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase';
+
+
+
+
 
 // function App() {
 class App extends react.Component{
-  // copying here now state
-    constructor () {
+  // // copying here now state
+  //   constructor () {
+  //   super();
+  //   this.state = {
+  //     products: [
+  //       {
+  //         price: 99,
+  //         title: 'Watch',
+  //         qty: 1,
+  //         img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  //         id: 1
+  //       },
+  //       {
+  //         price: 999,
+  //         title: 'Mobile Phone',
+  //         qty: 10,
+  //         img: 'https://images.unsplash.com/photo-1520923642038-b4259acecbd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1306&q=80',
+  //         id: 2
+  //       },
+  //       {
+  //         price: 999,
+  //         title: 'Laptop',
+  //         qty: 4,
+  //         img: 'https://images.unsplash.com/photo-1504707748692-419802cf939d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1330&q=80',
+  //         id: 3
+  //       }
+  //     ]
+  //   }
+  //   // this.increaseQuantity = this.increaseQuantity.bind(this);
+  //   // this.testing();
+  // }
+  constructor () {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-          id: 1
-        },
-        {
-          price: 999,
-          title: 'Mobile Phone',
-          qty: 10,
-          img: 'https://images.unsplash.com/photo-1520923642038-b4259acecbd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1306&q=80',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 4,
-          img: 'https://images.unsplash.com/photo-1504707748692-419802cf939d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1330&q=80',
-          id: 3
-        }
-      ]
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
+        products: [],
+        loading: true
+    }  // this.testing();
   }
+
+  componentDidMount () {
+    firebase
+      .firestore()
+      .collection('products')
+      .get()
+      .then((snapshot) => {
+      console.log(snapshot);
+
+      snapshot.docs.map((doc) => {
+        console.log(doc.data())
+      })
+
+      const products = snapshot.docs.map((doc) => {
+        const data = doc.data();
+
+        data['id']= doc.id;
+        return data;
+      })
+
+      this.setState({
+        products,
+        loading: false
+      })
+    })
+  }
+
   handleIncreaseQuantity = (product) => {
     console.log('Heyy please inc the qty of ', product);
     const { products } = this.state;
@@ -100,13 +139,17 @@ class App extends react.Component{
     let cartTotal = 0;
 
     products.map((product) => {
-      cartTotal = cartTotal + product.qty * product.price
+      if(product.qty>0){
+        cartTotal = cartTotal + product.qty * product.price;
+      }
+      return '';
+      
     })
 
     return cartTotal;
   }
   render () {
-    const { products } = this.state;
+    const { products, loading } = this.state;
   return (
     <div className="App">
     <Navbar count={this.getCartCount()} />
@@ -116,6 +159,7 @@ class App extends react.Component{
       onDecreaseQuantity={this.handleDecreaseQuantity}
       onDeleteProduct={this.handleDeleteProduct}
     />
+    {loading && <h1>Loading Products...</h1>}
     <div style={ {padding: 10, fontSize: 20} }>TOTAL: {this.getCartTotal()} </div>
   </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase';
+import 'firebase/firestore';
+const firebaseConfig = {
+  apiKey: "AIzaSyDUxgxjG9IKnX4tyFZKY7ZWnRgCqz08nN8",
+  authDomain: "cart-6b92c.firebaseapp.com",
+  projectId: "cart-6b92c",
+  storageBucket: "cart-6b92c.appspot.com",
+  messagingSenderId: "415332330240",
+  appId: "1:415332330240:web:112faa255bc16779cfa1e5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +21,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
